13bddaa7ff641f24fb0030a09bb0a7f06b7e477f
AaronScott2025 <147019715+AaronScott2025@users.noreply.github.com>
2024-07-30 00:57:15 -0400

    MAJOR UPDATE. QOL FOR FUTURE CODE, OVERHAUL OF UI, FRESH THOUGHTS FOR FUTURE DEV-ing
    -Overhauled the UI for index
    -Made css and javascript into their own files for ease of coding
    -Added pictures
    -Changed headings
    -Removed dropdown, replaced with simpler layout in heading
    -Reused "fade scroll" code from previous website attempt
    - Updated most styles in CSS

---
+++ scripts.js
@@ -0,0 +1,21 @@
+ //Fading Text in JS
+document.addEventListener('DOMContentLoaded', () => {
+  const fadeInSection = document.querySelectorAll('.fade-in-text');
+
+  const fadeInOutObserver = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+       if (entry.isIntersecting) {
+         entry.target.style.opacity = 1;
+         entry.target.style.transform = 'translateY(0)';
+       } else {
+         entry.target.style.opacity = 0;
+         entry.target.style.transform = 'translateY(20px)';
+       }
+     });
+   }, { threshold: 0.5 });
+
+  fadeInSection.forEach(section => {
+    fadeInOutObserver.observe(section);
+  });
+});
+
